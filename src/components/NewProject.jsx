c0433fb6456ef61206b1d1f644edd0fe59472de3
SimoCPZ <simonecapezzani@gmail.com>
2024-01-09 12:54:35 +0100

    Styling Input and NewProject

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,15 +1,15 @@
 import Input from "./Input";
 
 export default function NewProject() {
-    return <div>
-        <menu>
+    return <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
             <li>
-                <button>
+                <button className="text-stone-800 hover:text-stone-950">
                     Cancel
                 </button>
             </li>
             <li>
-                <button>
+                <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-800">
                     Save
                 </button>
             </li>
