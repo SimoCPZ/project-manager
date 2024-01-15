0bd5dba29d74bd6e03848a447cb526d54185e416
SimoCPZ <simonecapezzani@gmail.com>
2024-01-16 00:18:37 +0100

    Add project feature - missing UI update

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,24 +1,48 @@
 import Input from "./Input";
+import { useRef } from "react";
 
-export default function NewProject() {
-    return <div className="w-[35rem] mt-16">
-        <menu className="flex items-center justify-end gap-4 my-4">
-            <li>
-                <button className="text-stone-800 hover:text-stone-950">
-                    Cancel
-                </button>
-            </li>
-            <li>
-                <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-800">
-                    Save
-                </button>
-            </li>
-        </menu>
-        <div>
-            <Input label="Title" />
-            {/* Could set textarea = true but it's redundant */}
-            <Input label="Description" textarea />
-            <Input label="Due Date" />
+export default function NewProject({ onAdd }) {
+
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+
+
+    function handleSave() {
+        const enteredTitle = title.current.value;
+        const enteredDescription = description.current.value;
+        const enteredDueDate = dueDate.current.value;
+
+        //missing validation...
+
+        onAdd({
+            title: enteredTitle,
+            description: enteredDescription,
+            dueDate: enteredDueDate
+        });
+    }
+
+    return (
+        <div className="w-[35rem] mt-16">
+            <menu className="flex items-center justify-end gap-4 my-4">
+                <li>
+                    <button className="text-stone-800 hover:text-stone-950">
+                        Cancel
+                    </button>
+                </li>
+                <li>
+                    <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-800"
+                        onClick={handleSave}>
+                        Save
+                    </button>
+                </li>
+            </menu>
+            <div>
+                <Input type="text" ref={title} label="Title" />
+                {/* Could set textarea = true but it's redundant */}
+                <Input ref={description} label="Description" textarea />
+                <Input type="date" ref={dueDate} label="Due Date" />
+            </div>
         </div>
-    </div>
+    );
 }
