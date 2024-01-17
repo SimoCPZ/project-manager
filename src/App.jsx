2d733a164f81616438c29ac3614487cb9e101cad
SimoCPZ <simonecapezzani@gmail.com>
2024-01-17 10:28:22 +0100

    Adding projects to the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -22,19 +22,20 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectsState(prevState => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       }
 
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject]
       }
     });
   }
 
-  console.log(projectsState);
 
   let content;
 
@@ -46,7 +47,9 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects} />
       {content}
     </main>
   );
